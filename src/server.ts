c01daf7971db4284d851e8c7092d043c760b85ea
Gias-git <giasuddin.programmer@gmail.com>
2025-07-02 20:52:22 +0600

    Moongodb-connect

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { Server } from 'http';
 import app from './app';
+import mongoose from 'mongoose';
 
 
 let server: Server;
@@ -9,12 +10,14 @@ const PORT = 7000;
 async function main() {
 
     try {
+        await mongoose.connect('mongodb+srv://moongoose:0vN9S556P03tz88c@cluster0.vv2sz9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log("Moongoose Setup")
         server = app.listen(PORT, () => {
             console.log(`App Is listening in port : ${PORT}`)
         })
 
     } catch (error) {
-
+        console.log(error)
     }
 }
 
